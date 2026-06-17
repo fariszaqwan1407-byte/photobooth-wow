dc5b0d7b0751a4433ec3c577806a9fc2502da642
fariszaqwan1407-byte <fariszaqwan1407@gmail.com>
2026-06-17 17:10:58 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+let currentImage = "";
+
+/* CAMERA */
+async function startCamera(){
+  const video = document.getElementById("video");
+  const stream = await navigator.mediaDevices.getUserMedia({ video:true });
+  video.srcObject = stream;
+}
+
+function capture(){
+  const video = document.getElementById("video");
+  const canvas = document.getElementById("canvas");
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(video,0,0);
+
+  currentImage = canvas.toDataURL("image/jpeg",0.9);
+  document.getElementById("preview").src = currentImage;
+}
+
+/* UPLOAD */
+function uploadImage(){
+  fetch("YOUR_SCRIPT_URL",{
+    method:"POST",
+    body:JSON.stringify({ image:currentImage })
+  })
+  .then(r=>r.json())
+  .then(data=>{
+    window.location.href = "thankyou.html";
+  });
+}
+
+/* GALLERY */
+function loadGallery(){
+  fetch("YOUR_SCRIPT_URL?gallery=1")
+  .then(r=>r.json())
+  .then(data=>{
+    const box = document.getElementById("gallery");
+    data.images.forEach(url=>{
+      let img = document.createElement("img");
+      img.src = url;
+      box.appendChild(img);
+    });
+  });
+}
